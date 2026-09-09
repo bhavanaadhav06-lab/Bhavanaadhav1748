ba0f2a02347aa1e93c33ba0c45db8d92d9d23daf
bhavanaadhav06-lab <bhavanaadhav06@gmail.com>
2026-09-09 10:43:33 +0530

    Define Person, Staff, and Student classes with display method
    Fix constructor method names and implement polymorphism for Staff and Student classes.

--- Exp 01: Scenario No 10.py
+++ Exp 01: Scenario No 10.py
@@ -1,1 +1,39 @@
+class Person:
+    def _init_(self, name, address):
+        self.name = name
+        self.address = address
 
+    def display(self):
+        print(self.name, self.address)
+
+
+class Staff(Person):
+    def _init_(self, name, address, subject):
+        super()._init_(name, address)
+        self.subject = subject
+
+    def display(self):
+        print(self.name, self.address, self.subject)
+
+
+class Student(Person):
+    def _init_(self, name, address, grade):
+        super()._init_(name, address)
+        self.grade = grade
+
+    def display(self):
+        print(self.name, self.address, self.grade)
+
+
+s1 = Staff("Rahul", "Pune", "Python")
+s2 = Student("Aman", "Patna", "A")
+
+people = [s1, s2]
+
+for p in people:
+    p.display()       # Polymorphism
+
+print(isinstance(s1, Staff))
+print(isinstance(s2, Student))
+print(issubclass(Staff, Person))
+print(issubclass(Student, Person))
